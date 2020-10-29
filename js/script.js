fe6b7d80f9adaba452c4c3dccc981952f4d1f2d0
Volya S <olga.sharpmind@gmail.com>
2020-10-29 17:54:30 +0100

    Webp JS fixes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 let img = document.querySelectorAll('img');
 
 img.forEach((elem) => {
-        if (supportsWebp) {
-            elem.src = elem.getAttribute('data-webp');
-          } else {
-            elem.src = elem.getAttribute('data-otherImg');
-          }
-
+  if (supportsWebp) {
+    elem.src = elem.getAttribute('data-webp');
+  } else {
+    elem.src = elem.getAttribute('data-otherImg');
+  }
 });
 
 
 async function supportsWebp() {
-    if (!self.createImageBitmap) return false;
-    
-    const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
-    const blob = await fetch(webpData).then(r => r.blob());
-    return createImageBitmap(blob).then(() => true, () => false);
+  var elem = document.createElement('canvas');
+
+  if (!!(elem.getContext && elem.getContext('2d'))) {
+      // was able or not to get WebP representation
+      return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
   }
+
+  // very old browser like IE 8, canvas not supported
+  return false;
+}
